b55b7209023de9752e3a0ac4bb48e5132e31e0f9
JoTerrance <gsanz@iespabloserrano.com>
2025-10-18 18:07:34 +0200

    use copilot when suggestion menu is closed

--- lua/lsp/cmp.lua
+++ lua/lsp/cmp.lua
@@ -241,4 +241,10 @@ vim.api.nvim_create_autocmd(
 autocmd FileType TelescopePrompt lua require('cmp').setup.buffer { enabled = false }
 --]]
 
+cmp.event:on("menu_opened", function()
+  vim.b.copilot_suggestion_hidden = true
+end)
 
+cmp.event:on("menu_closed", function()
+  vim.b.copilot_suggestion_hidden = false
+end)

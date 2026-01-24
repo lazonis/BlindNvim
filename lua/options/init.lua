2cdcd6bfa5917f37a2fc9e3a9c2c420e0c64e8e9
zowi <xxx@example.com>
2026-01-24 16:12:13 +0100

    Add comments to options/init.lua -> spanish

--- lua/options/init.lua
+++ lua/options/init.lua
@@ -1,11 +1,12 @@
+--LÓGICA DE ACCESIBILIDAD--
 local has_braille = require("check-braille").has_braille_device()
 function BlindReturn(if_true, if_false)
   if has_braille then return if_true else return if_false end
 end
 -- [[ Setting options ]]
--- See `:help vim.o`
+-- LÓGICA INTERFAZ Y COMPORTAMIENTO
 vim.opt.completeopt = { "menu", "menuone", "noselect", "preview" }
--- Don't show the dumb matching stuff.
+-- Don't show the dumb matching stuff. 
 vim.opt.shortmess:append "c"
 vim.g.mapleader = "º"
 vim.g.maplocalleader = "º"
@@ -15,58 +16,114 @@ vim.o.breakindent = true
 vim.o.undofile = true
 -- Case insensitive searching UNLESS /C or capital in search
 vim.o.smartcase = true
+-- Command height -> reset command line to line 0
 vim.o.ch = 0
+-- Desactiva atajos por defecto
 vim.g.VM_default_mappings = 0
+-- Detección de tipo ed archivo para cargar plugins + reglas indentación
 vim.cmd('filetype plugin indent on')
+-- ???
 vim.o.shortmess = vim.o.shortmess .. 'c'
+-- Permite cambiar de archivo sin guardar, manteniendo su buffer en segundo plano
 vim.o.hidden = true
+-- Muestraa números relativos (10,9...0...9,10)
 vim.o.relativenumber= true
+--Salto de línea al mover el cursor izq/drch
 vim.o.whichwrap = 'b,s,<,>,[,],h,l'
+--Altura máxima menú autocompletado -> 10
 vim.o.pumheight = 10
+--Guarda archivos en formato UTF-8
 vim.o.fileencoding = 'utf-8'
+--Deja oculta la línea de comandos
 vim.o.cmdheight = 0
+--Al dividir la pantalla la nueva ventana aparece abajo
 vim.o.splitbelow = true
+
+--LÓGICA BLIND_RETURN
+-- Si usas braile -> abre pantalla a la izquierda (por qué?) REVISAR
+-- Si no usas braile -> abre pantalla flotante a la derecha
 vim.o.splitright = BlindReturn(false,true)
+
+-- Activa soporte para una gran variedad de colores
 vim.opt.termguicolors = true
+--¿?¿? MUESTRA TODO EL TEXTO -> Ámbito global
 vim.o.conceallevel = 0
+-- Muestra siempre la barra de pestañas arriba
 vim.o.showtabline = 2
+-- No muestra estado de archivo (INSERT, ETC) abajo del todo, lo hace LUA
 vim.o.showmode = false
+-- Evita crear archivos de seguridad que terminen en "~"
 vim.o.backup = false
+-- Evita crear archivos de seguridad que terminen en "~" (?) REVISAR
 vim.o.writebackup = false
+-- Tiempo de reacción del editor para mostrar errores o git updates
 vim.o.updatetime = 300
+-- Tiempo de reacción para ejecutar una combinación de teclas
 vim.o.timeoutlen = 100
+-- Conexión entre portapapeles de NEOVIM con los atajos Ctrl+C/Ctrol+V 
 vim.o.clipboard = "unnamedplus"
+-- Resaltación de palabras buscadas??? REVISAR
 vim.o.hlsearch = false
+-- Ignora mayúsculas por defecto
 vim.o.ignorecase = true
+-- Mantiene siempre 3 líneas de margen al hacer scroll vertical
 vim.o.scrolloff = 3
+-- Mantiene siempre 5 líneas de margen al hacer scroll horizontal
 vim.o.sidescrolloff = 5
+-- Activa el ratón en todos los modos (click, scroll, selection)
 vim.o.mouse = "a"
+
+-- Si tiene Braile -> Líneas largas se cortan visualmente
+-- Si no tiene Braile -> Líneas largas se salen de la pantalla, para no romper estructura
 vim.wo.wrap = BlindReturn(true, false)
+
+-- Activa columna de numeros a la izquierda
 vim.wo.number = true
+-- Resalta la línea donde está el cursor 
 vim.o.cursorline = true
+-- Muestra siempre la columnaa de signos (donde está error, git, bug, etce)
 vim.wo.signcolumn = "yes"
+
+-- Configuración del tabulador
+-- Vim.bo -> api que configura opciones de ámbito de búfer y de ventana
 vim.o.tabstop = 2
 vim.bo.tabstop = 2
 vim.o.softtabstop = 2
 vim.o.shiftwidth = 2
 vim.bo.shiftwidth = 2
+
+-- Al hacer enter la nueva línea mantiene la indentación de la anterior
 vim.o.autoindent = true
 vim.bo.autoindent = true
+
+--Convierte los tabs en espacios reales (evitar errores de formato)
 vim.o.expandtab = true
 vim.bo.expandtab = true
+
+
 vim.opt.termguicolors = true
+--Usa colores para fondos oscuros
 vim.o.background = 'dark'
+-- Establece para el archivo activo el nivel 2 -> escnder los caracteres de formato
 vim.opt_local.conceallevel = 2
+
+-- Si hay un error márcalo pero no añadas una linea nueva con la explicación
+-- Subsistema de diagnóstico -> vim.diagnostic -> modulo nativo neovim que actua como controlador de errores
+-- Recibe los datos de LSP y decide qué hacer
 vim.diagnostic.config({ virtual_lines = false })
+
+
 -- vim.cmd[[colorscheme tokyonight]]
 -- vim.g.tokyonight_style = 'night'
 -- [[ Highlight on yank ]]
 -- See `:help vim.highlight.on_yank()`
+
+--Creación de grupo de auto-comandos -> Cuando ocurra esto -> haz esto
 local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
-vim.api.nvim_create_autocmd('TextYankPost', {
+vim.api.nvim_create_autocmd('TextYankPost', { --Creas el evento específico
   callback = function()
-    vim.highlight.on_yank()
+    vim.highlight.on_yank() --Función nativa que pinta el fondo del texto seleccionado
   end,
   group = highlight_group,
-  pattern = '*',
+  pattern = '*', -- Se aplica a todos los tipos de archivos 
 })

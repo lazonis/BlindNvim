ab691aa0bfc9a22d56d9018772d57de4bc8b9aca
Kevin Miranda <moxixomiranda777@gmail.com>
2026-01-24 17:27:41 +0100

    Added comments
    Added comments to the lua/options/init.lua

--- lua/options/init.lua
+++ lua/options/init.lua
@@ -1,50 +1,30 @@
+
+
+-- Function to return different values based on the presence of a braille device
+
 local has_braille = require("check-braille").has_braille_device()
 function BlindReturn(if_true, if_false)
   if has_braille then return if_true else return if_false end
 end
--- [[ Setting options ]]
--- See `:help vim.o`
-vim.opt.completeopt = { "menu", "menuone", "noselect", "preview" }
--- Don't show the dumb matching stuff.
-vim.opt.shortmess:append "c"
+
+-- Key for general shortcuts
+
 vim.g.mapleader = "º"
 vim.g.maplocalleader = "º"
--- Enable break indent
-vim.o.breakindent = true
--- Save undo history
-vim.o.undofile = true
--- Case insensitive searching UNLESS /C or capital in search
-vim.o.smartcase = true
-vim.o.ch = 0
-vim.g.VM_default_mappings = 0
+
+-- Enable filetype detection and plugins
 vim.cmd('filetype plugin indent on')
-vim.o.shortmess = vim.o.shortmess .. 'c'
-vim.o.hidden = true
-vim.o.relativenumber= true
-vim.o.whichwrap = 'b,s,<,>,[,],h,l'
-vim.o.pumheight = 10
-vim.o.fileencoding = 'utf-8'
-vim.o.cmdheight = 0
-vim.o.splitbelow = true
-vim.o.splitright = BlindReturn(false,true)
-vim.opt.termguicolors = true
-vim.o.conceallevel = 0
-vim.o.showtabline = 2
-vim.o.showmode = false
-vim.o.backup = false
-vim.o.writebackup = false
-vim.o.updatetime = 300
-vim.o.timeoutlen = 100
-vim.o.clipboard = "unnamedplus"
-vim.o.hlsearch = false
-vim.o.ignorecase = true
-vim.o.scrolloff = 3
-vim.o.sidescrolloff = 5
+
+-- Mouse and clipboard settings
+
 vim.o.mouse = "a"
-vim.wo.wrap = BlindReturn(true, false)
-vim.wo.number = true
-vim.o.cursorline = true
-vim.wo.signcolumn = "yes"
+vim.o.clipboard = "unnamedplus"
+vim.o.fileencoding = 'utf-8'
+
+-- Tab and indetation settings
+
+vim.o.expandtab = true
+vim.bo.expandtab = true
 vim.o.tabstop = 2
 vim.bo.tabstop = 2
 vim.o.softtabstop = 2
@@ -52,16 +32,100 @@ vim.o.shiftwidth = 2
 vim.bo.shiftwidth = 2
 vim.o.autoindent = true
 vim.bo.autoindent = true
-vim.o.expandtab = true
-vim.bo.expandtab = true
+vim.o.breakindent = true
+
+-- Color and visual settings
+
 vim.opt.termguicolors = true
 vim.o.background = 'dark'
+vim.o.cursorline = true
+vim.o.conceallevel = 0
 vim.opt_local.conceallevel = 2
+
+-- Line numbers settings
+
+vim.wo.number = true
+vim.o.relativenumber= true
+
+-- Scroll margins settings
+
+vim.o.scrolloff = 3
+vim.o.sidescrolloff = 5
+
+-- Sings column settings
+
+vim.wo.signcolumn = "yes"
+
+-- Line wrap settings
+
+vim.wo.wrap = BlindReturn(true, false)
+
+-- Window split settings
+
+vim.o.splitbelow = true
+vim.o.splitright = BlindReturn(false,true)
+
+-- Command line height settings
+
+vim.o.cmdheight = 0
+vim.o.ch = 0
+
+-- Tab line settings
+
+vim.o.showtabline = 2
+
+-- Messages and modes settings
+
+vim.o.showmode = false
+vim.opt.shortmess:append "c"
+
+vim.o.ignorecase = true
+vim.o.smartcase = true
+vim.o.hlsearch = false
+
+-- Cursor movement settings when reaching line ends
+
+vim.o.whichwrap = 'b,s,<,>,[,],h,l'
+
+-- Suggestions and completions settings
+
+vim.opt.completeopt = { "menu", "menuone", "noselect", "preview" }
+vim.o.pumheight = 10
+
+-- File handling settings
+
+vim.o.hidden = true
+vim.o.undofile = true
+vim.o.backup = false
+vim.o.writebackup = false
+
+-- Times of refresh settings
+
+vim.o.updatetime = 300
+vim.o.timeoutlen = 100
+
+-- Diagnostic settings (LSP)
+
 vim.diagnostic.config({ virtual_lines = false })
+
+-- Theme settings
 -- vim.cmd[[colorscheme tokyonight]]
 -- vim.g.tokyonight_style = 'night'
 -- [[ Highlight on yank ]]
 -- See `:help vim.highlight.on_yank()`
+
+
+-- Visual Multi settings (specific plugin setting)
+
+vim.g.VM_default_mappings = 0
+
+-- Autocomplete menu setting 
+
+vim.o.shortmess = vim.o.shortmess .. 'c'
+
+
+-- Feedback Visual: Higlihght what you copy (Yank Highlight)
+
 local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
 vim.api.nvim_create_autocmd('TextYankPost', {
   callback = function()
